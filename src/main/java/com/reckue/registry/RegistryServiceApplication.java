35a877bd43e098f6530f45a2a14700c315404b36
Kamila M <camelya@mail.ru>
2020-07-27 16:15:05 +0300

    Add main class

---
+++ src/main/java/com/reckue/registry/RegistryServiceApplication.java
@@ -0,0 +1,18 @@
+package com.reckue.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Class RegistryServiceApplication represents a service for registration all microservices.
+ *
+ * @author Kamila Meshcheryakova
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class RegistryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryServiceApplication.class, args);
+    }
+}
